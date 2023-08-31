db4a7fb0e395adaa9cb07121398360682c7b9ad0
hannah <hannahscott@tutanota.com>
2023-08-31 17:30:22 +0100

    moves from static to docs

--- grow.sh
+++ grow.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # grow the garden
-mkdir -p static
+mkdir -p docs
+rm -r docs/*
 
 find garden -name "*.md" | cut -d/ -f2- | while read l; do
 	cat "garden/$l" | sed -E "s/\]\(garden/\]\(/g" > tmp.md
@@ -8,17 +9,17 @@ find garden -name "*.md" | cut -d/ -f2- | while read l; do
 
 	file=$(echo "$l" | rev | cut -d. -f2- | rev)
 	dir=$(echo "$l" | rev | cut -d/ -f2- | rev)
-	mkdir -p "./static/$dir"
-	pandoc -s tmp.md --lua-filter lazy.lua -o "./static/$file.html"
+	mkdir -p "./docs/$dir"
+	pandoc -s tmp.md --lua-filter lazy.lua -o "./docs/$file.html"
 	rm tmp.md
 done
 
 find garden -name "*.png" | cut -d/ -f2- | while read l; do
 	dir=$(echo "$l" | rev | cut -d/ -f2- | rev)
-	mkdir -p "./static/$dir"
-	cp "garden/$l" "./static/$l"
+	mkdir -p "./docs/$dir"
+	cp "garden/$l" "./docs/$l"
 done
 
-rm -r ./static/*.md
-rm -r ./static/.*
-rm -r ./static/private
+rm -r ./docs/*.md
+rm -r ./docs/.*
+rm -r ./docs/private
